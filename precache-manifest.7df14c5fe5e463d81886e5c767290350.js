71e7f1c8709ab6094345d534695ae9f871ea3bec
Peter Bengtsson <mail@peterbe.com>
2020-02-12 17:02:29 -0500

    Updates

---
+++ precache-manifest.7df14c5fe5e463d81886e5c767290350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c23e0bb6005337b3bb30d184917254f",
+    "url": "/mdn-sitetree/index.html"
+  },
+  {
+    "revision": "4b624881cffac6a12b0c",
+    "url": "/mdn-sitetree/static/css/main.22726399.chunk.css"
+  },
+  {
+    "revision": "8a57526fbb342f662ad1",
+    "url": "/mdn-sitetree/static/js/2.449cdb74.chunk.js"
+  },
+  {
+    "revision": "4b98f1517886ff0e9991641f4c0b0b88",
+    "url": "/mdn-sitetree/static/js/2.449cdb74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b624881cffac6a12b0c",
+    "url": "/mdn-sitetree/static/js/main.a4f4bea3.chunk.js"
+  },
+  {
+    "revision": "2ed52b65dc72be684a40",
+    "url": "/mdn-sitetree/static/js/runtime-main.a22e29bc.js"
+  }
+]);
